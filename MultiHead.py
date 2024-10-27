31e79f07e164d27c0d008a51f92d0c6e7ace89a2
Aivon99 <109174292+Aivon99@users.noreply.github.com>
2024-10-27 21:02:11 +0100

    first draft of multihead

---
+++ MultiHead.py
@@ -0,0 +1,21 @@
+import torch as tc
+import SelfBuilt as sb
+class MultiHead():
+    def __init__(self, h, d_model ): #projections d_k = d_v = d_model/h, in the paper's case 64 
+        super().__init__()
+ 
+        self.W_O = tc.nn.Linear(d_model, d_model, bias = False) # d_model x d_model
+        self.W_Q = tc.nn.Linear(d_model, d_model/h, bias = False) # d_model x d_k 
+        self.W_K = tc.nn.Linear(d_model, d_model/h, bias = False) # d_model x d_k
+        self.W_V = tc.nn.Linear(d_model, d_model/h, bias = False) # d_model x d_v
+
+        
+    def forward(self, V, K, Q):
+        
+        Q_p = tc.matmul(self.W_Q, Q)
+        V_p = tc.matmul(self.W_V, V)
+        K_p = tc.matmul(self.W_k, K)
+
+        return tc.matmul(sb.ScaleDotProduct(Q_p, K_p, V_p))
+
+    #not needed to define get/set methods pytorch nn tracks the parameters (thank god) 
--- SelfBuilt.py
+++ SelfBuilt.py
@@ -1,10 +1,12 @@
 import torch as tc
 
-def ScaleDotProduct(Q, K, V, flag):
-    QK = tc.matmul(Q, tc.transpose(K)) 
-    d_k = len(Q) #add equivalent of .length() function 
-    QK = QK/(d_k**(1/2))
-    scores = tc.softmax(QK) #TODO: implement masking, padding and cheating
+def ScaleDotProduct(Q, K, V, flag = "none"):
+    if flag != "none":
+        QK = tc.matmul(Q, tc.transpose(K)) 
+        d_k = len(Q) #add equivalent of .length() function 
+        QK = QK/(d_k**(1/2))
+        scores = tc.softmax(QK) #TODO: implement masking, padding and cheating
 
-    return tc.matmul(scores, V)
-
+        return tc.matmul(scores, V)
+    else 
+        return 0
