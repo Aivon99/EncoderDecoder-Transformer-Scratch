b932f842f6f32a828a6572f0d003a057352dc27d
Aivon99 <109174292+Aivon99@users.noreply.github.com>
2024-10-27 19:40:58 +0100

    small things

--- SelfBuilt.py
+++ SelfBuilt.py
@@ -4,7 +4,7 @@ def ScaleDotProduct(Q, K, V, flag):
     QK = tc.matmul(Q, tc.transpose(K)) 
     d_k = len(Q) #add equivalent of .length() function 
     QK = QK/(d_k**(1/2))
-    scores = tc.softmax(QK) #TODO: use the flag var to do masking (or not) finish buir
+    scores = tc.softmax(QK) #TODO: implement masking, padding and cheating
 
     return tc.matmul(scores, V)
 
